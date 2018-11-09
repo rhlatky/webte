dd2e8e64bb7a5b8d8ead18dc89a39df04a16c88f
Radoslav Hlatky <radoslav.hlatky@oneguard.email>
2018-11-09 19:33:33 +0100

    Test commit 2

--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+//import About from './views/About.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -14,6 +15,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
+    //  component: About
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
