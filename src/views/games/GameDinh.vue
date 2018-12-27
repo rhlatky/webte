909ab6d1a35bacf010fc9029aa4202e922899d61
Mr-SvaRoq <bibo.dnt.159@gmail.com>
2018-12-28 00:26:51 +0100

    Blank map of Madagascar has been assembeld Blank flag of Madagascar has been assembeld

--- src/views/games/GameDinh.vue
+++ src/views/games/GameDinh.vue
@@ -2,20 +2,34 @@
     <div id="mainContainer">
         <h1>Dinh Nguyen Tien Game 111111a</h1>
 
-        <div class="con" id="drops">
-            <div id="map-Antananarivo" v-on:drop="drop" v-on:dragover="allowDrop"></div>
-            <div id="map-Antisiranana" v-on:drop="drop" v-on:dragover="allowDrop"></div>
-            <div id="map-Fianarantsoa" v-on:drop="drop" v-on:dragover="allowDrop"></div>
-            <div id="map-Mahajanga" v-on:drop="drop" v-on:dragover="allowDrop"></div>
-            <div id="map-Toamasin" v-on:drop="drop" v-on:dragover="allowDrop"></div>
-            <div id="map-Toliara" v-on:drop="drop" v-on:dragover="allowDrop"></div>
+        <div class="con" id="map">
+            <div id="map-Antananarivo"></div>
+            <div id="map-Antisiranana"></div>
+            <div id="map-Fianarantsoa"></div>
+            <div id="map-Mahajanga"></div>
+            <div id="map-Toamasin"></div>
+            <div id="map-Toliara"></div
+            >
+        </div>
+
+        <div id="flag">
+            <div id="flag-white"></div>
+            <div id="flag-red"></div>
+            <div id="flag-green"></div>
 
         </div>
 
-        <div>
-            <div id="flag-green" v-on:drop="drop" v-on:dragover="allowDrop"></div>
-            <div id="flag-white" v-on:drop="drop" v-on:dragover="allowDrop"></div>
-            <div id="flag-red" v-on:drop="drop" v-on:dragover="allowDrop"></div>
+        <div >
+            <div id="flag-white-drag">aaaaa</div>
+            <div id="flag-red-drag">aaa aa</div>
+            <div id="flag-green-drag">asvasv</div>
+            <div id="map-Antananarivo-drag">asf asf </div>
+            <div id="map-Antisiranana-drag">fas f as</div>
+            <div id="map-Fianarantsoa-drag">f asf asf asf</div>
+            <div id="map-Mahajanga-drag"> fsdga d a</div>
+            <div id="map-Toamasin-drag">ag as </div>
+            <div id="map-Toliara-drag">a fas </div>
+
         </div>
 
     </div>
@@ -30,7 +44,16 @@
 </script>
 
 <style scoped>
+
+    #map{
+        /*margin-left: 572px;*/
+        margin-top: 200px;
+        /*border: 2px solid pink;*/
+    }
+
+
     #map-Antananarivo {
+        position: static;
         background-image: url("imgDinh/AntananarivoBlank.png");
         width: 110px;
         height: 110px;
@@ -40,6 +63,7 @@
     }
 
     #map-Antisiranana {
+        position: static;
         background-image: url("imgDinh/antisirananaBlank.png");
         width: 110px;
         height: 180px;
@@ -49,6 +73,7 @@
     }
 
     #map-Antisiranana {
+        position: static;
         background-image: url("imgDinh/antisirananaBlank.png");
         width: 110px;
         height: 180px;
@@ -58,6 +83,7 @@
     }
 
     #map-Fianarantsoa {
+        position: static;
         background-image: url("imgDinh/FianarantsoaBlank.png");
         width: 150px;
         height: 180px;
@@ -67,6 +93,7 @@
     }
 
     #map-Mahajanga{
+        position: static;
         background-image: url("imgDinh/MahajangaBlank.png");
         width: 230px;
         height: 228px;
@@ -76,6 +103,7 @@
     }
 
     #map-Toamasin{
+        position: static;
         background-image: url("imgDinh/ToamasinaBlank.png");
         width: 100px;
         height: 250px;
@@ -85,6 +113,7 @@
     }
 
     #map-Toliara{
+        position: static;
         background-image: url("imgDinh/ToliaraBlank.png");
         width: 180px;
         height: 310px;
@@ -93,16 +122,45 @@
         /*border: 2px solid red;*/
     }
 
+    #flag{
+        position: relative;
+        /*border: 2px solid red;*/
+        margin-left: 250px;
+        margin-top: -250px;
+    }
     #flag-white{
-        background-image: url("imgDinh/flag1Blank.png");
-        width: 180px;
-        height: 310px;
-        margin-left: 600px;
-        margin-top: 0px;
-        border: 2px solid red;
+        position: static;
+
+        background-image: url("imgDinh/flag1Blankrs.png");
+        width: 95px;
+        height: 185px;
+        margin-left: 611px;
+        margin-top: -98px;
+        /*border: 2px solid blue;*/
+    }
+
+    #flag-red{
+        position: static;
+        background-image: url("imgDinh/flag2Blankrs.png");
+        width: 185px;
+        height: 95px;
+        margin-left: 700px;
+        margin-top: -185px;
+        /*border: 2px solid red;*/
+    }
+
+    #flag-green{
+        position: static;
+        background-image: url("imgDinh/flag3Blankrs.png");
+        width: 185px;
+        height: 95px;
+        margin-left: 700px;
+        margin-top: -6px;
+        /*border: 2px solid green;*/
     }
 
     /*#map-part69 {*/
+        /*position: fixed;*/
         /*background-image: url("imgDinh/Blank.png");*/
         /*width: 500px;*/
         /*height: 600px;*/
