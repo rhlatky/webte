461eba37d3db35dbdd61ccdc72b582b5986baf07
rhlatky <radoslav.hlatky@oneguard.email>
2019-01-04 00:58:45 +0100

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-    <div>
-        <v-app>
-            <app-navbar></app-navbar>
-            <v-breadcrumbs :items="breadCrumbList" mode="out-in">
-                <v-icon slot="divider">forward</v-icon>
-            </v-breadcrumbs>
-            <v-fade-transition mode="out-in">
-                <router-view/>
-            </v-fade-transition>
-        </v-app>
-    </div>
+    <v-app>
+        <app-navbar></app-navbar>
+        <v-breadcrumbs :items="breadCrumbList" mode="out-in">
+            <v-icon slot="divider">forward</v-icon>
+        </v-breadcrumbs>
+        <v-fade-transition mode="out-in">
+            <router-view/>
+        </v-fade-transition>
+        <v-footer app height="auto">
+            <v-card class="flex" flat tile>
+                <v-card-actions class="blue lighten-3 justify-center">
+                    &copy;2018 — <strong>Vuetify</strong>
+                </v-card-actions>
+            </v-card>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
@@ -41,5 +46,9 @@
 
     #toolbar a:hover {
         text-decoration: none;
+    }
+
+    html {
+        overflow-y: auto;
     }
 </style>
