2ddb44ebc00a9389762e913961f845dd7cbb4e9d
tmh96 <hungtranminh96@gmail.com>
2019-01-04 02:14:08 +0100

    shows number of visits (alerts on pageload, should be working correctly)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,7 +10,46 @@
 
 <script>
     export default {
-        name: "Footer"
+        name: "Footer",
+        mounted:function(){
+            this.showVisits() //showVisits will execute at pageload
+        },
+        methods: {
+            showVisits: function showVisits() {
+
+                function setCookie(cname,cvalue,exdays) {
+                    var d = new Date();
+                    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+                    var expires = "expires=" + d.toGMTString();
+                    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+                }
+                function getCookie(cname) {
+                    var name = cname + "=";
+                    var decodedCookie = decodeURIComponent(document.cookie);
+                    var ca = decodedCookie.split(';');
+                    for(var i = 0; i < ca.length; i++) {
+                        var c = ca[i];
+                        while (c.charAt(0) == ' ') {
+                            c = c.substring(1);
+                        }
+                        if (c.indexOf(name) == 0) {
+                            return c.substring(name.length, c.length);
+                        }
+                    }
+                    return "";
+                }
+
+                var numLoads = parseInt(getCookie('pageLoads'));
+
+                if (isNaN(numLoads) || numLoads <= 0) {
+                    setCookie('pageLoads', 1);
+                }
+                else {
+                    setCookie('pageLoads', numLoads + 1);
+                }
+                alert("Number of Visits: " + getCookie('pageLoads'));
+            }
+        }
     }
 </script>
 
