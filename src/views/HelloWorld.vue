12e9e69dee739475b60e0c7bc768784794e8bbe3
tmh96 <hungtranminh96@gmail.com>
2019-01-04 17:52:50 +0100

    updated pagecount again

--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -1,151 +1,104 @@
 <template>
-  <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div class="mainCon">
+    <h1>Checklist</h1>
+    <table id="checklist">
+      <tr>
+        <th class="headers"></th>
+        <th class="headers">Radoslav Hlatký</th>
+        <th class="headers">Dinh Nguyen Tien</th>
+        <th class="headers">Hung Tran Minh</th>
+      </tr>
+      <tr>
+        <td class="headers">Trojúrovňové menu</td>
+        <td class="checkMark">&#10004;</td>
+        <td class="checkMark"></td>
+        <td class="checkMark"></td>
+      </tr>
+      <tr>
+        <td class="headers">Zobrazenie aktuálneho dátumu s meninami v daný deň</td>
+        <td class="checkMark"></td>
+        <td class="checkMark"></td>
+        <td class="checkMark"></td>
+      </tr>
+      <tr>
+        <td class="headers">Počítadlo osobných prístupov na stránku</td>
+        <td class="checkMark"></td>
+        <td class="checkMark"></td>
+        <td class="checkMark">&#10004;</td>
+      </tr>
+      <tr>
+        <td class="headers">Histórie prehliadania vo forme path-based breadcrumbs</td>
+        <td class="checkMark">&#10004;</td>
+        <td class="checkMark"></td>
+        <td class="checkMark"></td>
+      </tr>
+      <tr>
+        <td class="headers">Články v troch stĺpcoch vedľa seba</td>
+        <td class="checkMark"></td>
+        <td class="checkMark">&#10004;</td>
+        <td class="checkMark"></td>
+      </tr>
+      <tr>
+        <td class="headers">Individuálna úloha, hra</td>
+        <td class="checkMark">&#10004;</td>
+        <td class="checkMark">&#10004;</td>
+        <td class="checkMark">&#10004;</td>
+      </tr>
+      <tr>
+        <td class="headers">Kontakt na všetkých autorov</td>
+        <td class="checkMark"></td>
+        <td class="checkMark">&#10004;</td>
+        <td class="checkMark"></td>
+      </tr>
+      <tr>
+        <td class="headers">Checklist</td>
+        <td class="checkMark"></td>
+        <td class="checkMark"></td>
+        <td class="checkMark">&#10004;</td>
+      </tr>
+      <tr>
+        <td class="headers">Grafický návrh</td>
+        <td class="checkMark">&#10004;</td>
+        <td class="checkMark">&#10004;</td>
+        <td class="checkMark">&#10004;</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-import { VContainer, VLayout, VFlex, VImg } from "vuetify/lib";
 export default {
-  components: {
-    VContainer,
-    VLayout,
-    VFlex,
-    VImg
-  },
-  data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com"
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com"
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuetifyjs.com"
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/layout/pre-defined"
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
-  })
+    name: "HelloWorld"
 };
 </script>
 
-<style>
+<style scoped>
+  h1 {
+    text-align: center;
+    padding: 25px;
+  }
+  #checklist{
+    border-collapse: collapse;
+    margin: 0 auto;
+  }
+  .headers {
+    padding: 12px;
+    text-align: left;
+    background-color: #f44336;
+    color: white;
+    font-weight: bold;
+  }
+  tr, td {
+    border: 1px solid #d2d2d2;
+    padding: 8px;
+
+  }
+  .checkMark {
+    text-align: center;
+    font-size: 21px;
+    color: #52c556;
+  }
+  tr:nth-child(even){
+    background-color: #eaeaea;
+  }
 </style>
