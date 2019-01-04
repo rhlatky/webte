57516361c0805881a193ed39aacdcadad1d66ecf
tmh96 <hungtranminh96@gmail.com>
2019-01-04 14:56:38 +0100

    updated pagecount again

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,6 +16,7 @@
         },
         methods: {
             showVisits: function showVisits() {
+
                 function setCookie(cname,cvalue,exdays) {
                     var d = new Date();
                     d.setTime(d.getTime() + (exdays*24*60*60*1000));
